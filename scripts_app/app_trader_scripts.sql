a4d11b1c5c4d0a30eb88ca9343a4e41322a0a6bf
rneyin <ronej2001@yahoo.com>
2024-02-24 12:00:11 -0600

    completed

--- scripts_app/app_trader_scripts.sql
+++ scripts_app/app_trader_scripts.sql
@@ -130,4 +130,41 @@ app_store_apps
 -- ALTER TABLE play_store_dup
 -- ALTER COLUMN price TYPE numeric(5,2) USING price::numeric
 
-
+SELECT sub.name, primary_genre, ROUND(AVG(sub.rating), 1) AS avg_rating, size_bytes, CAST(sub.price AS MONEY), install_count, CAST(sub.review_count AS INTEGER), sub.rating, age, ROUND(longevity, 0) AS years_of_viability, MONEY(longevity*9000*12)-sub.purchase_price AS total_profit
+FROM
+	(SELECT name, ROUND(AVG(rating), 1) AS avg_rating, size_bytes, CAST(price AS MONEY), CAST(review_count AS INTEGER), rating, age, primary_genre, purchase_price,
+	CASE
+		WHEN ROUND(AVG(rating)) <= 0.49
+		THEN 1
+		ELSE ROUND(AVG(rating))/0.50+1
+		END AS longevity
+	FROM
+		(SELECT name, size_bytes, CAST(price AS MONEY), CAST(review_count AS INTEGER), rating, content_rating AS age, primary_genre,
+		CASE WHEN MONEY(price) BETWEEN '$0' AND '$1'
+			THEN '$10,000'
+			ELSE MONEY(price)*10000
+			END AS purchase_price
+		FROM public.app_store_apps
+		WHERE price <=1.00
+			AND rating IS NOT NULL
+		 AND rating >= 4.5
+			AND CAST(review_count AS INTEGER)>= 10000
+		GROUP BY name, size_bytes, CAST(price AS MONEY), CAST(review_count AS INTEGER), age, primary_genre, purchase_price, rating
+		UNION
+		SELECT name, size, MONEY(price), review_count, rating, content_rating, genres,
+		CASE WHEN MONEY(price) BETWEEN '$0' AND '$1'
+			THEN '$10,000'
+			ELSE MONEY(price)*10000
+			END AS purchase_price
+		FROM public.play_store_apps
+		WHERE MONEY(price) <='$1.00'
+			AND rating IS NOT NULL
+		  AND rating >= 4.5
+			AND review_count >= 10000
+		GROUP BY name, size, MONEY(price), review_count, content_rating, genres, purchase_price, rating)
+	GROUP BY name, size_bytes, CAST(price AS MONEY), CAST(review_count AS INTEGER), rating, age, primary_genre, purchase_price) AS sub
+INNER JOIN play_store_apps
+USING(name)
+WHERE install_count IS NOT NULL
+GROUP BY sub.name, primary_genre, size_bytes, CAST(sub.price AS MONEY), install_count, CAST(sub.review_count AS INTEGER), sub.rating, age, purchase_price, longevity, total_profit
+ORDER BY total_profit DESC, install_count  --review_count DESC;
