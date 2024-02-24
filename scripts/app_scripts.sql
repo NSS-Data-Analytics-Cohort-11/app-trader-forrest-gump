7b10bab131b221c56928b2a7373e84d1f1262304
Katie Clay <katie.clayy@outlook.com>
2024-02-24 12:03:04 -0600

    completed assignment

--- scripts/app_scripts.sql
+++ scripts/app_scripts.sql
@@ -135,34 +135,84 @@ FROM
 	WHERE rating IS NOT NULL)
 GROUP BY app_name, app_purchase_price
 ORDER BY avg_rating DESC;
+---
+SELECT *
+FROM app_store_apps;
 
+SELECT *
+FROM play_store_apps;
 
---Marketing price per year
-SELECT app_name, app_purchase_price, ROUND(AVG(rating), 1) AS avg_rating,
-	CASE WHEN ROUND(AVG(rating)) < 0.49
+--Marketing price per year... failed 
+SELECT sub.app_name, sub.genres, sub.age, ROUND(AVG(sub.avg_rating),1) AS avg_rating, sub.app_purchase_price, ROUND(sub.longevity,0), sub.marketing_fee_per_year
+FROM
+	(SELECT app_name, genres, age, app_purchase_price, ROUND(AVG(rating), 1) AS avg_rating,
+		CASE WHEN ROUND(AVG(rating)) < 0.49
+			THEN 1
+			ELSE ROUND(AVG(rating))/0.50+1
+			END AS longevity,
+		MONEY(9000)*12 AS marketing_fee_per_year
+	FROM
+		(SELECT name AS app_name, genres, MONEY(price),rating, content_rating AS age, 'play_store' AS app,
+			CASE WHEN MONEY(price) BETWEEN '$0' AND '$1'
+			THEN '$10,000'
+			ELSE MONEY(price)*10000
+			END AS app_purchase_price
+		FROM play_store_apps
+		WHERE rating IS NOT NULL
+		UNION
+		SELECT name AS app_name, primary_genre, MONEY(price), rating, content_rating, 'app_store' AS app,
+			CASE WHEN MONEY(price) BETWEEN '$0' AND '$1'
+			THEN '$10,000'
+			ELSE MONEY(price)*10000
+			END AS app_purchase_price
+		FROM app_store_apps
+		WHERE rating IS NOT NULL
+		GROUP BY app_name, app_purchase_price, content_rating, primary_genre, MONEY(price), rating)
+	GROUP BY sub.app_name, sub.genres, sub.age, sub.app_purchase_price) AS sub
+		
+	
+	
+	 
+	 
+	 
+--Final Query for analysis
+SELECT sub.name, primary_genre, ROUND(AVG(sub.rating), 1) AS avg_rating, size_bytes, CAST(sub.price AS MONEY), install_count, CAST(sub.review_count AS INTEGER), sub.rating, age, ROUND(longevity, 0) AS years_of_viability, MONEY(longevity*9000*12)-sub.purchase_price AS total_profit
+FROM
+	(SELECT name, ROUND(AVG(rating), 1) AS avg_rating, size_bytes, CAST(price AS MONEY), CAST(review_count AS INTEGER), rating, age, primary_genre, purchase_price,
+	CASE
+		WHEN ROUND(AVG(rating)) <= 0.49
 		THEN 1
 		ELSE ROUND(AVG(rating))/0.50+1
-		END AS longevity,
-	MONEY(9000)*12 AS marketing_fee_per_year
-FROM
-	(SELECT name AS app_name, genres, MONEY(price), rating, 'play_store' AS app,
+		END AS longevity
+	FROM
+		(SELECT name, size_bytes, CAST(price AS MONEY), CAST(review_count AS INTEGER), rating, content_rating AS age, primary_genre,
 		CASE WHEN MONEY(price) BETWEEN '$0' AND '$1'
-		THEN '$10,000'
-		ELSE MONEY(price)*10000
-		END AS app_purchase_price
-	FROM play_store_apps
-	WHERE rating IS NOT NULL
-	UNION
-	SELECT name AS app_name, primary_genre, MONEY(price), rating, 'app_store' AS app,
+			THEN '$10,000'
+			ELSE MONEY(price)*10000
+			END AS purchase_price
+		FROM public.app_store_apps
+		WHERE price <=1.00
+			AND rating IS NOT NULL
+		 AND rating >= 4.5
+			AND CAST(review_count AS INTEGER)>= 10000
+		GROUP BY name, size_bytes, CAST(price AS MONEY), CAST(review_count AS INTEGER), age, primary_genre, purchase_price, rating
+		UNION
+		SELECT name, size, MONEY(price), review_count, rating, content_rating, genres,
 		CASE WHEN MONEY(price) BETWEEN '$0' AND '$1'
-		THEN '$10,000'
-		ELSE MONEY(price)*10000
-		END AS app_purchase_price
-	FROM app_store_apps
-	WHERE rating IS NOT NULL)
-GROUP BY app_name, app_purchase_price, marketing_fee_per_year
-ORDER BY avg_rating DESC
-	 
-	 
+			THEN '$10,000'
+			ELSE MONEY(price)*10000
+			END AS purchase_price
+		FROM public.play_store_apps
+		WHERE MONEY(price) <='$1.00'
+			AND rating IS NOT NULL
+		 AND rating >= 4.5
+			AND review_count >= 10000
+		GROUP BY name, size, MONEY(price), review_count, content_rating, genres, purchase_price, rating)
+	GROUP BY name, size_bytes, CAST(price AS MONEY), CAST(review_count AS INTEGER), rating, age, primary_genre, purchase_price) AS sub
+INNER JOIN play_store_apps
+USING(name)
+WHERE install_count IS NOT NULL
+GROUP BY sub.name, primary_genre, size_bytes, CAST(sub.price AS MONEY), install_count, CAST(sub.review_count AS INTEGER), sub.rating, age, purchase_price, longevity, total_profit
+ORDER BY total_profit DESC, install_count --review_count DESC;
 	 
 	 
