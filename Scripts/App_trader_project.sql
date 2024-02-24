330989cc20293e237b32dae349e0a94b85dc73b2
zdemeke <dzenash@gmail.com>
2024-02-23 22:58:46 -0600

    working on it

--- Scripts/App_trader_project.sql
+++ Scripts/App_trader_project.sql
@@ -4,66 +4,43 @@ FROM app_store_apps
 SELECT * 
 FROM play_store_apps
 --change price and review_count columns data type.
-SELECT name, genres, MONEY(price),
-	CASE WHEN MONEY(price) BETWEEN '$0' AND '$1'
-	THEN '$10,000'
-	ELSE MONEY(price)*10000
---LIFESPAN
-SELECT name AS app_name, rating,
-	CASE WHEN rating BETWEEN 0 AND 0.4 THEN 1
-	WHEN rating BETWEEN 0.5 AND 0.9 THEN 2
-	WHEN rating BETWEEN 1 AND 1.4 THEN 3
-	WHEN rating BETWEEN 1.5 AND 1.9 THEN 4
-	WHEN rating BETWEEN 2 AND 2.4 THEN 5
-	WHEN rating BETWEEN 2.5 AND 2.9 THEN 6
-	WHEN rating BETWEEN 3 AND 3.4 THEN 7
-	WHEN rating BETWEEN 3.5 AND 3.9 THEN 8
-	WHEN rating BETWEEN 4 AND 4.4 THEN 9
-	WHEN rating BETWEEN 4.5 AND 4.9 THEN 10
-	WHEN rating = 5 THEN 11
-	END AS life_expectency
-FROM play_store_apps
-WHERE rating IS NOT NULL
 
+
+
+---SELECT name, genres, MONEY(price),
+	--CASE WHEN MONEY(price) BETWEEN '$0' AND '$1'
+	--THEN '$10,000'
+	--ELSE MONEY(price)*10000
+
+--Combine the two tables using UNION
 SELECT name, rating, primary_genre, review_count::integer, MONEY(price)
 FROM app_store_apps
 UNION 
 SELECT name, rating, genres, review_count, MONEY(price)
 FROM play_store_apps 
 
-
-SELECT apple.name, 
-  apple.price::Money AS apple_price,
-  apple.rating AS apple_rating,
-  (apple.rating + 1)* (5000 * 12) AS apple_revenue,
-  (apple.rating * 2)+1 AS apple_years
-FROM app_store_apps AS apple
-WHERE apple_revenue IS NOT NULL
---WHERE  apple.name ilike '%roblox%'
---ORDER BY apple_rating DESC
-UNION ALL
-SELECT play.name, 
-  play.price::Money AS play_price,
-  play.rating AS play_rating,
-  (play.rating + 1)* (5000 * 12) AS play_revenue,
-  (play.rating * 2)+1 AS play_years
-FROM play_store_apps AS play
-
-ORDER BY apple_revenue DESC
+--TRY TO GET LIFESPAN "don't use it"
+select
+		name, ROUND(AVG(rating),1) AS average_rating,
+			ROUND(avg(rating)*2, 0) AS lifespan
+from			
+		(SELECT name, rating, primary_genre, review_count::integer, MONEY(price)
+FROM app_store_apps
+UNION 
+SELECT name, rating, genres, review_count, MONEY(price)
+FROM play_store_apps ) as sub
+group by name
+--order by average_rating
 
 
 
-
---look for apps in both stores
-SELECT apple.name, apple.rating, MONEY(apple.price) As apple_money, MONEY(play.price) AS play_money
-FROM play_store_apps AS play
-INNER JOIN app_store_apps AS apple
-ON play.name = apple.name
 
 
 --calculate the cost
 -- Since monthly revenue is fixed, we need to calculate the lifespan of the app.(star rating *2)+1
 -- create  new columns for Revenu and Lifespan of apple store
+
+--TRYING TO CALCULATE REVENU (Don't use it, calculate the lifespan first)
 SELECT apple.name, 
 		apple.price::Money AS apple_price,
 		apple.rating AS apple_rating,
@@ -83,6 +60,54 @@ FROM play_store_apps AS play
 WHERE (play.rating + 1)* (5000 * 12) IS NOT NULL
 ORDER BY apple_revenue DESC
 
+-- Calculating purchase price:
+SELECT name AS app_name, genres, MONEY(price), rating, 'play_store' AS app,
+		CASE WHEN MONEY(price) BETWEEN '$0' AND '$1'
+		THEN '$10,000'
+		ELSE MONEY(price)*10000
+		END AS app_purchase_price
+	FROM play_store_apps
+	WHERE rating IS NOT NULL
+	UNION
+	SELECT name AS app_name, primary_genre, MONEY(price), rating, 'app_store' AS app,
+		CASE WHEN MONEY(price) BETWEEN '$0' AND '$1'
+		THEN '$10,000'
+		ELSE MONEY(price)*10000
+		END AS app_purchase_price
+	FROM app_store_apps
+	ORDER BY app_purchase_price
+	
+	
+	
+
+--look for apps in both stores
+SELECT apple.name, apple.rating, MONEY(apple.price) As apple_money, MONEY(play.price) AS play_money
+FROM play_store_apps AS play
+INNER JOIN app_store_apps AS apple
+ON play.name = apple.name
+
+
+--try to find which apps are in both tables from purchase query
+SELECT app_name, app
+FROM
+		 (SELECT name AS app_name, genres, MONEY(price), rating, 'play_store' AS app,
+		CASE WHEN MONEY(price) BETWEEN '$0' AND '$1'
+		THEN '$10,000'
+		ELSE MONEY(price)*10000
+		END AS app_purchase_price
+	FROM play_store_apps
+	WHERE rating IS NOT NULL
+	UNION
+	SELECT name AS app_name, primary_genre, MONEY(price), rating, 'app_store' AS app,
+		CASE WHEN MONEY(price) BETWEEN '$0' AND '$1'
+		THEN '$10,000'
+		ELSE MONEY(price)*10000
+		END AS app_purchase_price
+	FROM app_store_apps) AS sub
+	ORDER BY app_purchase_price
+
+
+---Longevity/lifespan
 SELECT name, ROUND(AVG(rating), 1) AS avg_rating,
 CASE
 	WHEN ROUND(AVG(rating)) < 0.49
@@ -176,7 +201,6 @@ FROM
 	WHERE rating IS NOT NULL)
 GROUP BY app_name, app_purchase_price
 ORDER BY avg_rating DESC;
-
 
 
 
